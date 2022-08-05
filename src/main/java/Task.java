1325de05027f658cef7f98b1af91e5bdaf779b94
Mert Ulas <Mert.Ulas@starfinanz.de>
2022-08-05 13:25:19 +0200

    task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,4 +7,44 @@ public class Task {
     private enum rating {easy, middle, hard}
     private int year;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
 }
